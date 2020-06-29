afd2593240b32c5c469b0fc9a82e16b19cfb1438
Roman <romantuser69@gmail.com>
2020-06-29 19:54:08 +0300

    Добавил больше значений для года

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Введите год в формате 2004");
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+
+        if ((year % 4 == 0) || (year % 400 == 0) || (year % 100 == 0)) {
+            System.out.println("Количество дней 366,год  високосный");
+
+        } else {
+            System.out.println("Количество дней 365 Год не високосный");
+
+
+        }
+    }
+}
